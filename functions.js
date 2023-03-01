5a18127e176957f98d5854aa328e3c7256737744
Maciek Uzi <uzi555555@gmail.com>
2023-03-01 17:38:25 +0100

    ADD JS text v2

--- functions.js
+++ functions.js
@@ -13,3 +13,21 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var computerMove;
 computerMove = 'papier';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+}
+if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+}
+printMessage('Twój ruch: ' + playerMove);
